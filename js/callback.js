3021f589d70580c67599fa4d05c4d65cc488a34e
Muhammed Anas <muhammedanas4140@gmail.com>
2018-02-23 17:53:55 +0530

    Callback in js

---
+++ js/callback.js
@@ -0,0 +1,31 @@
+window.onload = function() {
+	function handleError(jqXHR, textStatus, error) {
+		console.log(error)
+	}
+	$.ajax({
+		type: "GET",
+		url : "data/tweets.json",
+		success: cbTweets,
+		error: handleError
+	})
+	function cbTweets(data) {
+		console.log(data);
+		$.ajax({
+			type: "GET",
+			url : "data/friends.json",
+			success: cbFriends,
+			error: handleError
+		})
+		function cbFriends(data) {
+			console.log(data);
+			$.ajax({
+				type: "GET",
+				url : "data/videos.json",
+				success: function(data) {
+					console.log(data);
+				},
+				error: handleError
+			})
+		}
+		}
+}
