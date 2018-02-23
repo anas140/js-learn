9036506902fd3c8a785ec7de20cde53bae7e4956
Muhammed Anas <muhammedanas4140@gmail.com>
2018-02-23 15:08:55 +0530

    Async with jquery

--- js/async.js
+++ js/async.js
@@ -2,13 +2,18 @@ window.onload = function() {
 	var http = new XMLHttpRequest();
 	http.onreadystatechange = function() {
 		if(http.readyState == 4 && http.status ==200) {
-			console.log(JSON.parse(http.response));
-			console.log('first');
+			// console.log(JSON.parse(http.response));
+			// console.log('first');
 		}
 	}
 	http.open("GET", "data/tweets.json", true); 
 	http.send();
-	console.log('second');
+	// console.log('second');
+
+	/* Using Jquery  */
+	$.get("data/tweets.json", function(data) {
+		console.log(data);
+	})
 }
 
 
