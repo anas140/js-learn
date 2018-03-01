f0217843dd5ac55e7a244c8573a73c2835b4c8e4
Muhammed Anas <muhammedanas4140@gmail.com>
2018-03-02 00:39:39 +0530

    installed mocha package for testing and added one demo test

---
+++ test/demo_test.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+
+// Describe tests
+describe('some demo tests', function() {
+	// Create tests
+	it('add two number', function() {
+		assert(2 + 2 === 4);
+	})
+})
